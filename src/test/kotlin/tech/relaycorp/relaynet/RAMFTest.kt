1756b23d34ae68bc2a378b5e44760264ffd54413
Gus Narea <gnarea@users.noreply.github.com>
2020-03-04 12:07:37 +0000

    Complete POC example

--- src/test/kotlin/tech/relaycorp/relaynet/RAMFTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/RAMFTest.kt
@@ -1,6 +1,12 @@
 package tech.relaycorp.relaynet
 
+import com.beanit.jasn1.ber.ReverseByteArrayOutputStream
+import com.beanit.jasn1.ber.types.BerDateTime
+import com.beanit.jasn1.ber.types.BerInteger
+import com.beanit.jasn1.ber.types.BerOctetString
 import com.beanit.jasn1.ber.types.string.BerVisibleString
+import java.io.ByteArrayOutputStream
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,6 +15,15 @@ class RAMFTest {
     fun testSerializer() {
         val ramf = RAMF()
         ramf.recipient = BerVisibleString("04334")
+        ramf.messageId = BerVisibleString("message-id")
+        ramf.creationTimeUtc = BerDateTime("20191201183001")
+        ramf.ttl = BerInteger(1L)
+        ramf.payload = BerOctetString("payload".toByteArray())
+
+        val out = ReverseByteArrayOutputStream(1000)
+        ramf.encode(out)
+
+        File("/home/gus/tmp/ramf.der").writeBytes(out.array)
         assertEquals(BerVisibleString("04334").toString(), ramf.recipient!!.toString())
     }
 }
