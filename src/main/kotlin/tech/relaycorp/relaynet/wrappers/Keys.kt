f0044556da5e1a7df594fe1e429bf748f63e56b4
Gus Narea <gnarea@users.noreply.github.com>
2020-03-23 15:36:12 +0000

    Default to 2048-bit RSA keys + some refactoring

--- src/main/kotlin/tech/relaycorp/relaynet/wrappers/Keys.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/wrappers/Keys.kt
@@ -3,12 +3,15 @@ package tech.relaycorp.relaynet.wrappers
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 
+private const val DEFAULT_RSA_KEY_MODULUS = 2048
+private const val MIN_RSA_KEY_MODULUS = 2048
+
 @Throws(KeyException::class)
-fun generateRSAKeyPair(modulus: Int): KeyPair {
-    if (modulus < 2048) {
-        throw KeyException("The modulus should be at least 2048 (got $modulus)")
+fun generateRSAKeyPair(modulus: Int = DEFAULT_RSA_KEY_MODULUS): KeyPair {
+    if (modulus < MIN_RSA_KEY_MODULUS) {
+        throw KeyException("Modulus should be at least $MIN_RSA_KEY_MODULUS (got $modulus)")
     }
     val keyGen = KeyPairGenerator.getInstance("RSA")
-    keyGen.initialize(modulus) // `modulus` should be >= 2048 and default to 2048
+    keyGen.initialize(modulus)
     return keyGen.generateKeyPair()
 }
--- src/test/kotlin/tech/relaycorp/relaynet/wrappers/KeysTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/wrappers/KeysTest.kt
@@ -1,7 +1,8 @@
 package tech.relaycorp.relaynet.wrappers
 
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,9 +12,22 @@ class KeysTest {
     inner class GenerateRSAKeyPair {
         @Test
         fun `Key pair should be returned when a valid modulus is passed`() {
-            val keyPair = generateRSAKeyPair(2048)
-            assertNotNull(keyPair.private)
-            assertNotNull(keyPair.public)
+            val keyPair = generateRSAKeyPair(4096)
+
+            assert(keyPair.private is RSAPrivateKey)
+            assert(keyPair.private.toString().contains("4096 bits"))
+
+            assert(keyPair.public is RSAPublicKey)
+            assert(keyPair.public.toString().contains("4096 bits"))
+        }
+
+        @Test
+        fun `Modulus should be 2048 by default`() {
+            val keyPair = generateRSAKeyPair()
+
+            assert(keyPair.private.toString().contains("2048 bits"))
+
+            assert(keyPair.public.toString().contains("2048 bits"))
         }
 
         @Test
@@ -22,7 +36,7 @@ class KeysTest {
                 generateRSAKeyPair(2047)
             }
             assertEquals(
-                "The modulus should be at least 2048 (got 2047)",
+                "Modulus should be at least 2048 (got 2047)",
                 exception.message
             )
         }
