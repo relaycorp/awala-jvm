65302321773ad3533624adbff23a75fc7fb99a10
Gus Narea <gnarea@users.noreply.github.com>
2020-03-24 19:30:50 +0000

    chore(test): Fix test checking random behaviour

--- src/test/kotlin/tech/relaycorp/relaynet/wrappers/PRNGTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/wrappers/PRNGTest.kt
@@ -1,16 +1,22 @@
 package tech.relaycorp.relaynet.wrappers
 
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.RepeatedTest
 
 class PRNGTest {
     @Nested
     inner class GenerateRandomBigInteger {
-        @Test
-        fun `Output should be 64 bit number`() {
+        @RepeatedTest(8) // Because the bitLength of the value is variable
+        fun `Output should be 64 bit unsigned number`() {
             val value = generateRandomBigInteger()
 
-            assert(value.bitLength() in 61..64)
+            assertEquals(1, value.signum(), "Value should be positive")
+            assertTrue(
+                value.bitLength() in 56..64,
+                "Value should span up to 64 bits; got ${value.bitLength()}"
+            )
         }
     }
 }
