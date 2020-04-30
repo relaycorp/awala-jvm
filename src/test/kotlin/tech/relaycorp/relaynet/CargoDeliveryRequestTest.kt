6a223a7a0cb8a4facfa63f83413162201ef7e85c
Gus Narea <gnarea@users.noreply.github.com>
2020-04-30 11:47:08 +0100

    feat: Implement cargo delivery request

---
+++ src/test/kotlin/tech/relaycorp/relaynet/CargoDeliveryRequestTest.kt
@@ -0,0 +1,20 @@
+package tech.relaycorp.relaynet
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CargoDeliveryRequestTest {
+    @Test
+    fun `Fields should be set`() {
+        val localId = "id"
+        val cargoSerialized = "cargo"
+
+        val request = CargoDeliveryRequest(localId, cargoSerialized.byteInputStream())
+
+        assertEquals(localId, request.localId)
+        assertEquals(
+            cargoSerialized,
+            request.cargoSerialized.bufferedReader().use { it.readText() }
+        )
+    }
+}
