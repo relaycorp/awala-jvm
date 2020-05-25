f85e9f3dd8bc8b5b667ba730c64fcf66a190bb7d
Gus Narea <gnarea@users.noreply.github.com>
2020-05-25 16:15:38 +0100

    chore: Make generateRandomBigInteger() an internal function

--- src/main/kotlin/tech/relaycorp/relaynet/wrappers/PRNG.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/wrappers/PRNG.kt
@@ -3,7 +3,7 @@ package tech.relaycorp.relaynet.wrappers
 import java.math.BigInteger
 import java.security.SecureRandom
 
-fun generateRandomBigInteger(): BigInteger {
+internal fun generateRandomBigInteger(): BigInteger {
     val random = SecureRandom()
     return BigInteger(64, random)
 }
