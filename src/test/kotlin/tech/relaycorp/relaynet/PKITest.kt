4452110abe1fe4d29a50ab9664a292eb0684e099
Gus Narea <gnarea@users.noreply.github.com>
2020-07-08 12:12:11 +0100

    feat(PKI): Implement function to issue endpoint certificates (#65)

--- src/test/kotlin/tech/relaycorp/relaynet/PKITest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/PKITest.kt
@@ -124,4 +124,96 @@ class PKITest {
             assertEquals(1, basicConstraints.pathLenConstraint.toInt())
         }
     }
+
+    @Nested
+    inner class IssueEndpointCertificate {
+        @Test
+        fun `CommonName should be set to private address of gateway`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            assertEquals(certificate.subjectPrivateAddress, certificate.commonName)
+        }
+
+        @Test
+        fun `Subject public key should be honored`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            assertEquals(
+                keyPair.public.encoded.asList(),
+                certificate.certificateHolder.subjectPublicKeyInfo.encoded.asList()
+            )
+        }
+
+        @Test
+        fun `Issuer private key should be honored`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            val verifierProvider = JcaContentVerifierProviderBuilder().build(keyPair.public)
+            assertTrue(certificate.certificateHolder.isSignatureValid(verifierProvider))
+        }
+
+        @Test
+        fun `Validity end date should be honored`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            assertEquals(
+                tomorrow.toEpochSecond(),
+                certificate.certificateHolder.notAfter.toInstant().epochSecond
+            )
+        }
+
+        @Test
+        fun `Validity start date should be honored if set`() {
+            val startDate = ZonedDateTime.now().minusSeconds(30)
+            val certificate = issueEndpointCertificate(
+                keyPair.public,
+                keyPair.private,
+                tomorrow,
+                validityStartDate = startDate
+            )
+
+            assertEquals(
+                startDate.toEpochSecond(),
+                certificate.certificateHolder.notBefore.toInstant().epochSecond
+            )
+        }
+
+        @Test
+        fun `Issuer certificate should be honored if set`() {
+            val issuerKeyPair = generateRSAKeyPair()
+            val issuerCertificate =
+                issueEndpointCertificate(issuerKeyPair.public, issuerKeyPair.private, tomorrow)
+
+            val subjectCertificate = issueEndpointCertificate(
+                keyPair.public,
+                keyPair.private,
+                tomorrow,
+                issuerCertificate = issuerCertificate
+            )
+
+            val issuerCommonNames = subjectCertificate.certificateHolder.issuer.getRDNs(BCStyle.CN)
+            assertEquals(
+                issuerCertificate.commonName,
+                issuerCommonNames.first().first.value.toString()
+            )
+        }
+
+        @Test
+        fun `Subject should be marked as CA`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            assertTrue(
+                BasicConstraints.fromExtensions(certificate.certificateHolder.extensions).isCA
+            )
+        }
+
+        @Test
+        fun `pathLenConstraint should be 0`() {
+            val certificate = issueEndpointCertificate(keyPair.public, keyPair.private, tomorrow)
+
+            val basicConstraints =
+                BasicConstraints.fromExtensions(certificate.certificateHolder.extensions)
+            assertEquals(0, basicConstraints.pathLenConstraint.toInt())
+        }
+    }
 }
