c07f6c9ff4a32103f077b5687ce9830ce287f6a4
Gus Narea <gnarea@users.noreply.github.com>
2020-07-15 14:31:10 +0100

    feat(RAMF): Implement Parcel class

---
+++ src/main/kotlin/tech/relaycorp/relaynet/messages/Parcel.kt
@@ -0,0 +1,51 @@
+package tech.relaycorp.relaynet.messages
+
+import tech.relaycorp.relaynet.ramf.RAMFException
+import tech.relaycorp.relaynet.ramf.RAMFMessage
+import tech.relaycorp.relaynet.ramf.RAMFMessageCompanion
+import tech.relaycorp.relaynet.ramf.RAMFSerializer
+import tech.relaycorp.relaynet.wrappers.x509.Certificate
+import java.io.InputStream
+import java.time.ZonedDateTime
+
+private val SERIALIZER = RAMFSerializer(0x50, 0x00)
+
+/**
+ * Parcel
+ */
+class Parcel(
+    recipientAddress: String,
+    payload: ByteArray,
+    senderCertificate: Certificate,
+    messageId: String? = null,
+    creationDate: ZonedDateTime? = null,
+    ttl: Int? = null,
+    senderCertificateChain: Set<Certificate>? = null
+) : RAMFMessage(
+    SERIALIZER,
+    recipientAddress,
+    payload,
+    senderCertificate,
+    messageId,
+    creationDate,
+    ttl,
+    senderCertificateChain
+) {
+    companion object : RAMFMessageCompanion<Parcel> {
+        /**
+         * Deserialize parcel
+         */
+        @JvmStatic
+        @Throws(RAMFException::class)
+        override fun deserialize(serialization: ByteArray) =
+            SERIALIZER.deserialize(serialization, ::Parcel)
+
+        /**
+         * Deserialize parcel
+         */
+        @JvmStatic
+        @Throws(RAMFException::class)
+        override fun deserialize(serialization: InputStream) =
+            SERIALIZER.deserialize(serialization, ::Parcel)
+    }
+}
