833d5a1af2f0e63f79ab3b99663f59caa8a4a13b
Gus Narea <gnarea@users.noreply.github.com>
2020-08-06 12:22:45 +0100

    require cause in RelaynetException

--- src/main/kotlin/tech/relaycorp/relaynet/RelaynetException.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/RelaynetException.kt
@@ -3,5 +3,4 @@ package tech.relaycorp.relaynet
 /**
  * Base class for all exceptions in this library
  */
-abstract class RelaynetException(message: String, cause: Throwable? = null) :
-    Exception(message, cause)
+abstract class RelaynetException(message: String, cause: Throwable?) : Exception(message, cause)
