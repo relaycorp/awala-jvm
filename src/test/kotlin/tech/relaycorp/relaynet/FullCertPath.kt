01f69e9f76f1e888ee15face13369d3181684a78
Gus Narea <gnarea@users.noreply.github.com>
2020-08-21 16:22:57 +0100

    feat: Implement PDC interface (#84)

--- src/test/kotlin/tech/relaycorp/relaynet/FullCertPath.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/FullCertPath.kt
@@ -3,29 +3,35 @@ package tech.relaycorp.relaynet
 import java.time.ZonedDateTime
 
 object FullCertPath {
-    private val tomorrow = ZonedDateTime.now().plusDays(1)
+    private val now: ZonedDateTime = ZonedDateTime.now()
+    private val twoSecondsAgo = now.minusSeconds(2)
+    private val tomorrow = now.plusDays(1)
 
     val PUBLIC_GW = issueGatewayCertificate(
         KeyPairSet.PUBLIC_GW.public,
         KeyPairSet.PUBLIC_GW.private,
-        tomorrow
+        tomorrow,
+        validityStartDate = twoSecondsAgo
     )
     val PRIVATE_GW = issueGatewayCertificate(
         KeyPairSet.PRIVATE_GW.public,
         KeyPairSet.PUBLIC_GW.private,
         tomorrow,
-        PUBLIC_GW
+        PUBLIC_GW,
+        twoSecondsAgo
     )
     val PRIVATE_ENDPOINT = issueEndpointCertificate(
         KeyPairSet.PRIVATE_ENDPOINT.public,
         KeyPairSet.PRIVATE_GW.private,
         tomorrow,
-        PRIVATE_GW
+        PRIVATE_GW,
+        twoSecondsAgo
     )
     val PDA = issueParcelDeliveryAuthorization(
         KeyPairSet.PDA_GRANTEE.public,
         KeyPairSet.PRIVATE_ENDPOINT.private,
         tomorrow,
-        PRIVATE_ENDPOINT
+        PRIVATE_ENDPOINT,
+        twoSecondsAgo
     )
 }
