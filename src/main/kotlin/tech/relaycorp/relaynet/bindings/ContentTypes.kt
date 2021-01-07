55bc540c02ed3241038bfff4839e81090e9774e5
Gus Narea <gnarea@users.noreply.github.com>
2021-01-07 10:27:56 +0000

    improve names

--- src/main/kotlin/tech/relaycorp/relaynet/bindings/ContentTypes.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/bindings/ContentTypes.kt
@@ -6,8 +6,9 @@ package tech.relaycorp.relaynet.bindings
 @Suppress("unused")
 enum class ContentTypes(val value: String) {
     PARCEL("application/vnd.relaynet.parcel"),
-    REGISTRATION_AUTHORIZATION("application/vnd.relaynet.node-registration.authorization"),
-    REGISTRATION_REQUEST("application/vnd.relaynet.node-registration.request"),
-    REGISTRATION("application/vnd.relaynet.node-registration.registration"),
-    PRE_REGISTRATION("text/plain")
+
+    NODE_REGISTRATION_AUTHORIZATION("application/vnd.relaynet.node-registration.authorization"),
+    NODE_REGISTRATION_REQUEST("application/vnd.relaynet.node-registration.request"),
+    NODE_REGISTRATION("application/vnd.relaynet.node-registration.registration"),
+    NODE_PRE_REGISTRATION("text/plain")
 }
