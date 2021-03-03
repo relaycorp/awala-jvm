4aa6864e75752400cd2475db575f5ff217340172
Gus Narea <gnarea@users.noreply.github.com>
2021-03-03 16:37:00 +0000

    feat: Implement key pair deserialisation (#104)
    Fixes #66.

--- src/main/kotlin/tech/relaycorp/relaynet/wrappers/Keys.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/wrappers/Keys.kt
@@ -2,15 +2,21 @@
 
 package tech.relaycorp.relaynet.wrappers
 
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey
+import org.bouncycastle.jce.spec.ECPublicKeySpec
 import tech.relaycorp.relaynet.BC_PROVIDER
 import tech.relaycorp.relaynet.ECDHCurve
 import tech.relaycorp.relaynet.getSHA256DigestHex
 import java.security.KeyFactory
 import java.security.KeyPair
 import java.security.KeyPairGenerator
+import java.security.PrivateKey
 import java.security.PublicKey
+import java.security.interfaces.RSAPrivateCrtKey
 import java.security.spec.ECGenParameterSpec
 import java.security.spec.InvalidKeySpecException
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.RSAPublicKeySpec
 import java.security.spec.X509EncodedKeySpec
 
 private const val DEFAULT_RSA_KEY_MODULUS = 2048
@@ -36,6 +42,41 @@ fun generateRSAKeyPair(modulus: Int = DEFAULT_RSA_KEY_MODULUS): KeyPair {
     val keyGen = KeyPairGenerator.getInstance("RSA", BC_PROVIDER)
     keyGen.initialize(modulus)
     return keyGen.generateKeyPair()
+}
+
+/**
+ * Deserialize the RSA key pair from a private key serialization.
+ */
+@Throws(KeyException::class)
+fun ByteArray.deserializeRSAKeyPair(): KeyPair {
+    val privateKey = this.deserializePrivateKey("RSA") as RSAPrivateCrtKey
+    val keyFactory = KeyFactory.getInstance("RSA", BC_PROVIDER)
+    val publicKeySpec = RSAPublicKeySpec(privateKey.modulus, privateKey.publicExponent)
+    val publicKey = keyFactory.generatePublic(publicKeySpec)
+    return KeyPair(publicKey, privateKey)
+}
+
+/**
+ * Deserialize the EC key pair from a private key serialization.
+ */
+@Throws(KeyException::class)
+fun ByteArray.deserializeECKeyPair(): KeyPair {
+    val privateKey = this.deserializePrivateKey("EC") as BCECPrivateKey
+    val keyFactory = KeyFactory.getInstance("EC", BC_PROVIDER)
+    val curvePublicPoint = privateKey.parameters.g.multiply(privateKey.d)
+    val publicKeySpec = ECPublicKeySpec(curvePublicPoint, privateKey.parameters)
+    val publicKey = keyFactory.generatePublic(publicKeySpec)
+    return KeyPair(publicKey, privateKey)
+}
+
+private fun ByteArray.deserializePrivateKey(algorithm: String): PrivateKey {
+    val privateKeySpec = PKCS8EncodedKeySpec(this)
+    val keyFactory = KeyFactory.getInstance(algorithm, BC_PROVIDER)
+    return try {
+        keyFactory.generatePrivate(privateKeySpec)
+    } catch (exc: InvalidKeySpecException) {
+        throw KeyException("Value is not a valid $algorithm private key", exc)
+    }
 }
 
 fun ByteArray.deserializeRSAPublicKey() = deserializePublicKey("RSA")
