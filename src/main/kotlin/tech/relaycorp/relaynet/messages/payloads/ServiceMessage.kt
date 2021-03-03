7117867a04b513887ed98df1aa4ccbebd4bf70b0
Gus Narea <gnarea@users.noreply.github.com>
2021-03-03 22:56:57 +0000

    implement deserialisation

--- src/main/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessage.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessage.kt
@@ -2,6 +2,8 @@ package tech.relaycorp.relaynet.messages.payloads
 
 import org.bouncycastle.asn1.DEROctetString
 import org.bouncycastle.asn1.DERVisibleString
+import tech.relaycorp.relaynet.messages.InvalidMessageException
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Exception
 import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 
 class ServiceMessage(val type: String, val content: ByteArray) : EncryptedPayload() {
@@ -9,5 +11,24 @@ class ServiceMessage(val type: String, val content: ByteArray) : EncryptedPayloa
         val typeASN1 = DERVisibleString(type)
         val contentASN1 = DEROctetString(content)
         return ASN1Utils.serializeSequence(arrayOf(typeASN1, contentASN1), false)
+    }
+
+    companion object {
+        @Throws(InvalidMessageException::class)
+        fun deserialize(serialization: ByteArray): ServiceMessage {
+            val sequence = try {
+                ASN1Utils.deserializeHeterogeneousSequence(serialization)
+            } catch (exc: ASN1Exception) {
+                throw InvalidMessageException("Service message is not a DER sequence", exc)
+            }
+            if (sequence.size < 2) {
+                throw InvalidMessageException(
+                    "Service message sequence should have at least two items (got ${sequence.size})"
+                )
+            }
+            val type = ASN1Utils.getVisibleString(sequence.first())
+            val content = ASN1Utils.getOctetString(sequence[1])
+            return ServiceMessage(type.string, content.octets)
+        }
     }
 }
--- src/test/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessageTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessageTest.kt
@@ -1,9 +1,14 @@
 package tech.relaycorp.relaynet.messages.payloads
 
+import org.bouncycastle.asn1.DERNull
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.assertThrows
+import tech.relaycorp.relaynet.messages.InvalidMessageException
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Exception
 import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 const val TYPE = "this is the type"
 val CONTENT = "this is the content".toByteArray()
@@ -31,6 +36,47 @@ internal class ServiceMessageTest {
             val sequence = ASN1Utils.deserializeHeterogeneousSequence(serialization)
             val contentASN1 = ASN1Utils.getOctetString(sequence[1])
             assertEquals(CONTENT.asList(), contentASN1.octets.asList())
+        }
+    }
+
+    @Nested
+    inner class Deserialize {
+        @Test
+        fun `Serialization should be DER sequence`() {
+            val invalidSerialization = "foo".toByteArray()
+
+            val exception = assertThrows<InvalidMessageException> {
+                ServiceMessage.deserialize(invalidSerialization)
+            }
+
+            assertEquals("Service message is not a DER sequence", exception.message)
+            assertTrue(exception.cause is ASN1Exception)
+        }
+
+        @Test
+        fun `Sequence should have at least two items`() {
+            val invalidSerialization =
+                ASN1Utils.serializeSequence(arrayOf(DERNull.INSTANCE), false)
+
+            val exception = assertThrows<InvalidMessageException> {
+                ServiceMessage.deserialize(invalidSerialization)
+            }
+
+            assertEquals(
+                "Service message sequence should have at least two items (got 1)",
+                exception.message
+            )
+        }
+
+        @Test
+        fun `Valid service message should be accepted`() {
+            val message = ServiceMessage(TYPE, CONTENT)
+            val serialization = message.serializePlaintext()
+
+            val messageDeserialized = ServiceMessage.deserialize(serialization)
+
+            assertEquals(TYPE, messageDeserialized.type)
+            assertEquals(CONTENT.asList(), messageDeserialized.content.asList())
         }
     }
 }
