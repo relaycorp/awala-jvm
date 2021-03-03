e83d9ae0235afd580e802abc15a6bf8f3e7472a1
Gus Narea <gnarea@users.noreply.github.com>
2021-03-03 22:48:33 +0000

    feat: Implement ServiceMessage

--- src/main/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessage.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessage.kt
@@ -1,7 +1,13 @@
 package tech.relaycorp.relaynet.messages.payloads
 
-class ServiceMessage : EncryptedPayload() {
+import org.bouncycastle.asn1.DEROctetString
+import org.bouncycastle.asn1.DERVisibleString
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
+
+class ServiceMessage(val type: String, val content: ByteArray) : EncryptedPayload() {
     override fun serializePlaintext(): ByteArray {
-        TODO("Not yet implemented")
+        val typeASN1 = DERVisibleString(type)
+        val contentASN1 = DEROctetString(content)
+        return ASN1Utils.serializeSequence(arrayOf(typeASN1, contentASN1), false)
     }
 }
--- src/test/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessageTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/messages/payloads/ServiceMessageTest.kt
@@ -1,12 +1,36 @@
 package tech.relaycorp.relaynet.messages.payloads
 
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Nested
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 import kotlin.test.Test
+import kotlin.test.assertEquals
+
+const val TYPE = "this is the type"
+val CONTENT = "this is the content".toByteArray()
 
 internal class ServiceMessageTest {
-    @Test
-    fun deserialize() {
-        // TODO
-        assertThrows<NotImplementedError> { ServiceMessage().serializePlaintext() }
+    @Nested
+    inner class SerializePlaintext {
+        @Test
+        fun `Type should be serialized`() {
+            val message = ServiceMessage(TYPE, CONTENT)
+
+            val serialization = message.serializePlaintext()
+
+            val sequence = ASN1Utils.deserializeHeterogeneousSequence(serialization)
+            val typeASN1 = ASN1Utils.getVisibleString(sequence.first())
+            assertEquals(TYPE, typeASN1.string)
+        }
+
+        @Test
+        fun `Content should be serialized`() {
+            val message = ServiceMessage(TYPE, CONTENT)
+
+            val serialization = message.serializePlaintext()
+
+            val sequence = ASN1Utils.deserializeHeterogeneousSequence(serialization)
+            val contentASN1 = ASN1Utils.getOctetString(sequence[1])
+            assertEquals(CONTENT.asList(), contentASN1.octets.asList())
+        }
     }
 }
