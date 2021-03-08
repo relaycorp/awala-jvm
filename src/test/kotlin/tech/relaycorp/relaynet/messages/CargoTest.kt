80b49c23066c795e4cdf7bdfb975a3db928c74a3
Gus Narea <gnarea@users.noreply.github.com>
2021-03-08 21:27:00 +0000

    fix(Parcel): Decrypt encapsulated service messages (#108)

--- src/test/kotlin/tech/relaycorp/relaynet/messages/CargoTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/messages/CargoTest.kt
@@ -1,6 +1,7 @@
 package tech.relaycorp.relaynet.messages
 
-import tech.relaycorp.relaynet.CERTIFICATE
+import tech.relaycorp.relaynet.CDACertPath
+import tech.relaycorp.relaynet.KeyPairSet
 import tech.relaycorp.relaynet.messages.payloads.CargoMessageSet
 import tech.relaycorp.relaynet.ramf.RAMFSpecializationTestCase
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
@@ -17,9 +18,13 @@ internal class CargoTest : RAMFSpecializationTestCase<Cargo>(
     @Test
     fun `Payload deserialization should be delegated to CargoMessageSet`() {
         val cargoMessageSet = CargoMessageSet(arrayOf("msg1".toByteArray(), "msg2".toByteArray()))
-        val cargo = Cargo("https://gb.relaycorp.tech", "".toByteArray(), CERTIFICATE)
+        val cargo = Cargo(
+            "https://gb.relaycorp.tech",
+            cargoMessageSet.encrypt(CDACertPath.PUBLIC_GW),
+            CDACertPath.PRIVATE_GW
+        )
 
-        val payloadDeserialized = cargo.deserializePayload(cargoMessageSet.serializePlaintext())
+        val payloadDeserialized = cargo.unwrapPayload(KeyPairSet.PUBLIC_GW.private)
 
         assertEquals(
             cargoMessageSet.messages.map { it.asList() },
--- src/test/kotlin/tech/relaycorp/relaynet/messages/ParcelTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/messages/ParcelTest.kt
@@ -1,10 +1,12 @@
 package tech.relaycorp.relaynet.messages
 
-import org.junit.jupiter.api.assertThrows
-import tech.relaycorp.relaynet.CERTIFICATE
+import tech.relaycorp.relaynet.KeyPairSet
+import tech.relaycorp.relaynet.PDACertPath
+import tech.relaycorp.relaynet.messages.payloads.ServiceMessage
 import tech.relaycorp.relaynet.ramf.RAMFSpecializationTestCase
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class ParcelTest : RAMFSpecializationTestCase<Parcel>(
     ::Parcel,
@@ -15,9 +17,15 @@ internal class ParcelTest : RAMFSpecializationTestCase<Parcel>(
 ) {
     @Test
     fun `Payload deserialization should be delegated to ServiceMessage`() {
-        val parcel = Parcel("https://gb.relaycorp.tech", "".toByteArray(), CERTIFICATE)
+        val serviceMessage = ServiceMessage("the type", "the content".toByteArray())
+        val parcel = Parcel(
+            "https://gb.relaycorp.tech",
+            serviceMessage.encrypt(PDACertPath.PRIVATE_ENDPOINT),
+            PDACertPath.PDA
+        )
 
-        // TODO
-        assertThrows<NotImplementedError> { parcel.deserializePayload("invalid".toByteArray()) }
+        val serviceMessageDeserialized = parcel.unwrapPayload(KeyPairSet.PRIVATE_ENDPOINT.private)
+        assertEquals(serviceMessage.type, serviceMessageDeserialized.type)
+        assertEquals(serviceMessage.content.asList(), serviceMessageDeserialized.content.asList())
     }
 }
