03d63def7f23185c9d486bd7e916f2622833b17c
Gus Narea <gnarea@users.noreply.github.com>
2021-04-08 23:18:37 +0100

    chore: Fix formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -169,7 +169,9 @@ nexusPublishing {
     repositories {
         sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            snapshotRepositoryUrl.set(
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            )
             username.set(System.getenv("MAVEN_USERNAME"))
             password.set(System.getenv("MAVEN_PASSWORD"))
         }
