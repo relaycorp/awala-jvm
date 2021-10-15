fe2f389b8285ae4fbd90ab123ff3bda761818249
Gus Narea <gnarea@users.noreply.github.com>
2021-10-15 15:11:30 +0100

    fix(EnvelopedData): Set UKM when using key agreement
    For security reasons and because [PKI.js requires it](https://github.com/PeculiarVentures/PKI.js/issues/335).
    
    I couldn't write a test for the change in EnvelopedData due to https://github.com/bcgit/bc-java/issues/1043
    
    This work is part of https://github.com/relaycorp/relayverse/issues/27

--- src/main/kotlin/tech/relaycorp/relaynet/wrappers/PRNG.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/wrappers/PRNG.kt
@@ -7,3 +7,10 @@ internal fun generateRandomBigInteger(): BigInteger {
     val random = SecureRandom()
     return BigInteger(64, random)
 }
+
+internal fun generateRandomOctets(size: Int): ByteArray {
+    val random = SecureRandom()
+    val bytes = ByteArray(size)
+    random.nextBytes(bytes)
+    return bytes
+}
