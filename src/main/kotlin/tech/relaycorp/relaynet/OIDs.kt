701b7c77f0653b6deeae36aaa2c89862a66a4daa
Gus Narea <gnarea@users.noreply.github.com>
2021-10-16 16:50:06 +0100

    feat(SessionEnvelopedData): Include originator key id in unprotectedAttrs (#168)
    This is needed in https://github.com/relaycorp/relayverse/issues/27

--- src/main/kotlin/tech/relaycorp/relaynet/OIDs.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/OIDs.kt
@@ -5,13 +5,17 @@ import org.bouncycastle.asn1.ASN1ObjectIdentifier
 internal object OIDs {
     private val RELAYCORP: ASN1ObjectIdentifier =
         ASN1ObjectIdentifier("0.4.0.127.0.17").intern()
-    private val RELAYNET = RELAYCORP.branch("0").intern()
+    private val AWALA = RELAYCORP.branch("0").intern()
 
-    private val PRIVATE_NODE_REGISTRATION_PREFIX = RELAYNET.branch("2").intern()
+    private val AWALA_PKI = AWALA.branch("1").intern()
+    val ORIGINATOR_EPHEMERAL_CERT_SERIAL_NUMBER: ASN1ObjectIdentifier =
+        AWALA_PKI.branch("0")
+
+    private val PRIVATE_NODE_REGISTRATION_PREFIX = AWALA.branch("2").intern()
     val PNRA: ASN1ObjectIdentifier =
         PRIVATE_NODE_REGISTRATION_PREFIX.branch("0").intern()
     val PNRA_COUNTERSIGNATURE: ASN1ObjectIdentifier =
         PRIVATE_NODE_REGISTRATION_PREFIX.branch("1").intern()
 
-    val DETACHED_SIGNATURE: ASN1ObjectIdentifier = RELAYNET.branch("3").intern()
+    val DETACHED_SIGNATURE: ASN1ObjectIdentifier = AWALA.branch("3").intern()
 }
