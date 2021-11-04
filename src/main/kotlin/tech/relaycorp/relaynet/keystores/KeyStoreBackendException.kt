bd5a6edb724f01b5fdbaa6c1358d620f48387311
Gus Narea <gnarea@users.noreply.github.com>
2021-11-04 17:58:04 +0000

    fix(keystores): Let backends throw KeyStoreBackendException

--- src/main/kotlin/tech/relaycorp/relaynet/keystores/KeyStoreBackendException.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/keystores/KeyStoreBackendException.kt
@@ -2,5 +2,5 @@ package tech.relaycorp.relaynet.keystores
 
 import tech.relaycorp.relaynet.RelaynetException
 
-class KeyStoreBackendException internal constructor(message: String, cause: Throwable? = null) :
+class KeyStoreBackendException(message: String, cause: Throwable? = null) :
     RelaynetException(message, cause)
