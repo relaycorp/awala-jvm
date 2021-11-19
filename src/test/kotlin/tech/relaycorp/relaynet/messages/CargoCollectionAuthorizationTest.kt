75c758cae72c94926e1f7354d0855d491d4fb229
Gus Narea <gnarea@users.noreply.github.com>
2021-11-19 11:02:26 +0000

    fix(RAMF): Encrypt Cargo Collection Requests inside CCAs (#205)
    * fix(RAMF): Encrypt Cargo Collection Requests inside CCAs
    
    * delete UnencryptedRAMFMessage

--- src/test/kotlin/tech/relaycorp/relaynet/messages/CargoCollectionAuthorizationTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/messages/CargoCollectionAuthorizationTest.kt
@@ -1,10 +1,19 @@
 package tech.relaycorp.relaynet.messages
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.BeforeEach
+import tech.relaycorp.relaynet.SessionKeyPair
+import tech.relaycorp.relaynet.messages.payloads.CargoCollectionRequest
 import tech.relaycorp.relaynet.ramf.RAMFSpecializationTestCase
+import tech.relaycorp.relaynet.utils.CDACertPath
 import tech.relaycorp.relaynet.utils.ID_CERTIFICATE
+import tech.relaycorp.relaynet.utils.MockPrivateKeyStore
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class CargoCollectionAuthorizationTest :
     RAMFSpecializationTestCase<CargoCollectionAuthorization>(
         ::CargoCollectionAuthorization,
@@ -13,12 +22,36 @@ internal class CargoCollectionAuthorizationTest :
         0x00,
         CargoCollectionAuthorization.Companion
     ) {
-    @Test
-    fun `Payload deserialization should be delegated to EmptyPayload`() {
-        val cca = CargoCollectionAuthorization(
-            "https://gb.relaycorp.tech", "".toByteArray(), ID_CERTIFICATE
-        )
+    private val recipientSessionKeyPair = SessionKeyPair.generate()
+    private val senderSessionKeyPair = SessionKeyPair.generate()
 
-        cca.deserializePayload()
+    private val privateKeyStore = MockPrivateKeyStore()
+
+    @BeforeEach
+    fun registerSessionKey() = runBlockingTest {
+        privateKeyStore.saveSessionKey(
+            recipientSessionKeyPair.privateKey,
+            recipientSessionKeyPair.sessionKey.keyId,
+            CDACertPath.PRIVATE_GW.subjectPrivateAddress,
+            CDACertPath.PUBLIC_GW.subjectPrivateAddress,
+        )
     }
+
+    @Test
+    fun `Payload deserialization should be delegated to CargoCollectionRequest`() =
+        runBlockingTest {
+            val ccr = CargoCollectionRequest(CDACertPath.PUBLIC_GW)
+            val cca = CargoCollectionAuthorization(
+                CDACertPath.PUBLIC_GW.subjectPrivateAddress,
+                ccr.encrypt(recipientSessionKeyPair.sessionKey, senderSessionKeyPair),
+                ID_CERTIFICATE
+            )
+
+            val (payloadDeserialized) = cca.unwrapPayload(recipientSessionKeyPair.privateKey)
+
+            assertEquals(
+                ccr.cargoDeliveryAuthorization,
+                payloadDeserialized.cargoDeliveryAuthorization
+            )
+        }
 }
