9f8768ac70090283a83c984bef0ede460eb3509c
Gus Narea <gnarea@users.noreply.github.com>
2022-02-22 12:49:05 +0100

    implement serialisation

--- src/main/kotlin/tech/relaycorp/relaynet/messages/CertificateRotation.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/messages/CertificateRotation.kt
@@ -1,9 +1,32 @@
 package tech.relaycorp.relaynet.messages
 
+import org.bouncycastle.asn1.DEROctetString
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
 
-class CertificateRotation(subjectCertificate: Certificate, chain: List<Certificate>) {
+class CertificateRotation(val subjectCertificate: Certificate, val chain: List<Certificate>) {
     fun serialize(): ByteArray {
-        TODO("Not yet implemented")
+        val chainSequence = ASN1Utils.makeSequence(
+            chain.map { DEROctetString(it.serialize()) },
+            false
+        )
+        val sequence = ASN1Utils.serializeSequence(
+            listOf(
+                DEROctetString(subjectCertificate.serialize()),
+                chainSequence
+            ),
+            false
+        )
+        return FORMAT_SIGNATURE + sequence
+    }
+
+    companion object {
+        private const val concreteMessageType: Byte = 0x10
+        private const val concreteMessageVersion: Byte = 0
+        internal val FORMAT_SIGNATURE = byteArrayOf(
+            *"Relaynet".toByteArray(),
+            concreteMessageType,
+            concreteMessageVersion
+        )
     }
 }
