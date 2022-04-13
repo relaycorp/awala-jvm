57ecc8bf78844d914ba99e4fe0976f39b0593e53
Gus Narea <gnarea@users.noreply.github.com>
2022-04-13 16:26:12 +0100

    implement CertificationPath.deserialize

--- src/main/kotlin/tech/relaycorp/relaynet/pki/CertificationPath.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/pki/CertificationPath.kt
@@ -1,8 +1,11 @@
 package tech.relaycorp.relaynet.pki
 
 import org.bouncycastle.asn1.DEROctetString
+import org.bouncycastle.asn1.DERSequence
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Exception
 import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
+import tech.relaycorp.relaynet.wrappers.x509.CertificateException
 
 class CertificationPath(
     val leafCertificate: Certificate,
@@ -10,7 +13,45 @@ class CertificationPath(
 ) {
     fun serialize(): ByteArray {
         val leafCertificateASN1 = DEROctetString(leafCertificate.serialize())
-        val chainASN1 = ASN1Utils.makeSequence(chain.map { DEROctetString(it.serialize()) }, false)
+        val chainASN1 = ASN1Utils.makeSequence(chain.map { DEROctetString(it.serialize()) }, true)
         return ASN1Utils.serializeSequence(listOf(leafCertificateASN1, chainASN1), false)
+    }
+
+    companion object {
+        @Throws(CertificationPathException::class)
+        fun deserialize(serialization: ByteArray): CertificationPath {
+            val sequence = try {
+                ASN1Utils.deserializeHeterogeneousSequence(serialization)
+            } catch (exc: ASN1Exception) {
+                throw CertificationPathException("Path is not a valid DER sequence", exc)
+            }
+            if (sequence.size < 2) {
+                throw CertificationPathException("Path sequence should have at least 2 items")
+            }
+
+            val leafCertificateASN1 = ASN1Utils.getOctetString(sequence.first())
+            val leafCertificate = try {
+                Certificate.deserialize(leafCertificateASN1.octets)
+            } catch (exc: CertificateException) {
+                throw CertificationPathException("Leaf certificate is malformed", exc)
+            }
+
+            val chainSequence = try {
+                DERSequence.getInstance(sequence[1], false)
+            } catch (exc: IllegalStateException) {
+                throw CertificationPathException("Chain is malformed", exc)
+            }
+            val chain = try {
+                chainSequence.toList()
+                    .map { DEROctetString.getInstance(it).octets }
+                    .map { Certificate.deserialize(it) }
+            } catch (exc: CertificateException) {
+                throw CertificationPathException("Chain contains malformed certificate", exc)
+            } catch (exc: IllegalArgumentException) {
+                throw CertificationPathException("Chain contains non-OCTET STRING item", exc)
+            }
+
+            return CertificationPath(leafCertificate, chain)
+        }
     }
 }
--- src/test/kotlin/tech/relaycorp/relaynet/pki/CertificationPathTest.kt
+++ src/test/kotlin/tech/relaycorp/relaynet/pki/CertificationPathTest.kt
@@ -1,13 +1,18 @@
 package tech.relaycorp.relaynet.pki
 
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.bouncycastle.asn1.ASN1Sequence
-import org.bouncycastle.asn1.ASN1TaggedObject
+import org.bouncycastle.asn1.DERNull
+import org.bouncycastle.asn1.DEROctetString
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import tech.relaycorp.relaynet.utils.PDACertPath
+import tech.relaycorp.relaynet.wrappers.asn1.ASN1Exception
 import tech.relaycorp.relaynet.wrappers.asn1.ASN1Utils
 import tech.relaycorp.relaynet.wrappers.x509.Certificate
+import tech.relaycorp.relaynet.wrappers.x509.CertificateException
 
 class CertificationPathTest {
     @Nested
@@ -33,10 +38,120 @@ class CertificationPathTest {
             val sequence = ASN1Utils.deserializeHeterogeneousSequence(serialization)
             val chainSequence = ASN1Sequence.getInstance(sequence[1], false)
             assertEquals(1, chainSequence.size())
-            val caSerialized =
-                ASN1Utils.getOctetString(chainSequence.first() as ASN1TaggedObject).octets
+            val caSerialized = (chainSequence.first() as DEROctetString).octets
             val ca = Certificate.deserialize(caSerialized)
             assertEquals(PDACertPath.PRIVATE_ENDPOINT, ca)
+        }
+    }
+
+    @Nested
+    inner class Deserialize {
+        @Test
+        fun `Malformed sequence should be refused`() {
+            val serialization = "invalid".toByteArray()
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Path is not a valid DER sequence", exception.message)
+            assertTrue(exception.cause is ASN1Exception)
+        }
+
+        @Test
+        fun `Sequence should have at least two items`() {
+            val serialization = ASN1Utils.serializeSequence(
+                listOf(DEROctetString(PDACertPath.PDA.serialize())), false
+            )
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Path sequence should have at least 2 items", exception.message)
+        }
+
+        @Test
+        fun `Malformed leaf certificate should be refused`() {
+            val serialization = ASN1Utils.serializeSequence(
+                listOf(
+                    DEROctetString("malformed".toByteArray()),
+                    ASN1Utils.makeSequence(emptyList()),
+                ),
+                false
+            )
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Leaf certificate is malformed", exception.message)
+            assertTrue(exception.cause is CertificateException)
+        }
+
+        @Test
+        fun `Malformed chain should be refused`() {
+            val serialization = ASN1Utils.serializeSequence(
+                listOf(
+                    DEROctetString(PDACertPath.PDA.serialize()),
+                    DERNull.INSTANCE,
+                ),
+                false
+            )
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Chain is malformed", exception.message)
+            assertTrue(exception.cause is IllegalStateException)
+        }
+
+        @Test
+        fun `Malformed certificate in chain should be refused`() {
+            val serialization = ASN1Utils.serializeSequence(
+                listOf(
+                    DEROctetString(PDACertPath.PDA.serialize()),
+                    ASN1Utils.makeSequence(listOf(DEROctetString("malformed".toByteArray()))),
+                ),
+                false
+            )
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Chain contains malformed certificate", exception.message)
+            assertTrue(exception.cause is CertificateException)
+        }
+
+        @Test
+        fun `Malformed OCTET STRING in chain should be refused`() {
+            val serialization = ASN1Utils.serializeSequence(
+                listOf(
+                    DEROctetString(PDACertPath.PDA.serialize()),
+                    ASN1Utils.makeSequence(listOf(DERNull.INSTANCE)),
+                ),
+                false
+            )
+
+            val exception = assertThrows<CertificationPathException> {
+                CertificationPath.deserialize(serialization)
+            }
+
+            assertEquals("Chain contains non-OCTET STRING item", exception.message)
+            assertTrue(exception.cause is IllegalArgumentException)
+        }
+
+        @Test
+        fun `Valid serialization should be accepted`() {
+            val path = CertificationPath(PDACertPath.PDA, listOf(PDACertPath.PRIVATE_ENDPOINT))
+            val serialization = path.serialize()
+
+            val pathDeserialized = CertificationPath.deserialize(serialization)
+
+            assertEquals(PDACertPath.PDA, pathDeserialized.leafCertificate)
+            assertEquals(listOf(PDACertPath.PRIVATE_ENDPOINT), pathDeserialized.chain)
         }
     }
 }
